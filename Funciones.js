c147a5c4b14e061e06543e758bf26765d2d960a9
llTomasll <tomas28g@hotmail.com>
2020-06-16 16:45:46 -0300

    Calculos

--- Funciones.js
+++ Funciones.js
@@ -1,3 +1,26 @@
-function f() {
+function CalcularVoltaje() {
+    var num1,num2;
+
+    num1=Number(document.getElementsByName("ampv")[0].value);
+    num2=Number(document.getElementsByName("omhv")[0].value);
+    document.getElementsByName("Voltaje_Total") [0].innerHTML=num1*num2;
+
+}
+
+function CalcularCorriente() {
+    var num1,num2;
+
+    num1=Number(document.getElementsByName("voltc")[0].value);
+    num2=Number(document.getElementsByName("ohmc")[0].value);
+    document.getElementsByName("Corriente_Total") [0].innerHTML=num1/num2;
+
+}
+
+function CalcularResistencia() {
+    var num1,num2;
+
+    num1=Number(document.getElementsByName("voltr")[0].value);
+    num2=Number(document.getElementsByName("ampr")[0].value);
+    document.getElementsByName("Resistencia_Total") [0].innerHTML=num1/num2;
 
 }
