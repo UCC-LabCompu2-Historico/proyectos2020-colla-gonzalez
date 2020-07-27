acc9250b7a79e2242b5fa0e263d25a1c8a284a4f
llTomasll <tomas28g@hotmail.com>
2020-07-27 06:54:11 -0300

    Fixes

--- Funciones.js
+++ Funciones.js
@@ -1,25 +1,25 @@
 function CalcularVoltaje() {
     var num1,num2;
 
-    num1=Number(document.getElementsByName("ampv")[0].value); //Igualo num1 a ampv
-    num2=Number(document.getElementsByName("ohmv")[0].value); //Igualo num2 a ampv
-    document.getElementsByName("Volataje_Total") [0].innerHTML=Math.round((num1*num2)*100)/100; //Hago la multiplicacion
+    num1=Number(document.getElementsByName("ampv")[0].value);
+    num2=Number(document.getElementsByName("ohmv")[0].value);
+    document.getElementsByName("Voltaje_Total") [0].innerHTML=Math.round((num1*num2)*100)/100;
 }
 
 function CalcularCorriente() {
     var num1,num2;
 
-    num1=Number(document.getElementsByName("voltc")[0].value); //Igualo num1 a voltc
-    num2=Number(document.getElementsByName("ohmc")[0].value); //Igualo num2 a ohmc
-    document.getElementsByName("Corriente_Total") [0].innerHTML=Math.round((num1/num2)*100)/100 ; //Hago la division
+    num1=Number(document.getElementsByName("voltc")[0].value);
+    num2=Number(document.getElementsByName("ohmc")[0].value);
+    document.getElementsByName("Corriente_Total") [0].innerHTML=Math.round((num1/num2)*100)/100 ;
 }
 
 function CalcularResistencia() {
     var num1,num2;
 
-    num1=Number(document.getElementsByName("voltr")[0].value); //Igualo num2 a voltr
-    num2=Number(document.getElementsByName("ampr")[0].value); //Igualo num2 a ampr
-    document.getElementsByName("Resistencia_Total") [0].innerHTML=Math.round((num1/num2)*100)/100 //Hago la division
+    num1=Number(document.getElementsByName("voltr")[0].value);
+    num2=Number(document.getElementsByName("ampr")[0].value);
+    document.getElementsByName("Resistencia_Total") [0].innerHTML=Math.round((num1/num2)*100)/100
 }
 
 
