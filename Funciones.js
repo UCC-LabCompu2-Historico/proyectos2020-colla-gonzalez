f495eec2d7954bdce9cd53ff8d0da8537f42868a
llTomasll <tomas28g@hotmail.com>
2020-07-28 02:41:28 -0300

    Fixes

--- Funciones.js
+++ Funciones.js
@@ -1,28 +1,50 @@
+/**
+ * Calcula el voltaje
+ * @method CalcularVoltaje
+ */
 function CalcularVoltaje() {
     var num1,num2;
 
     num1=Number(document.getElementsByName("ampv")[0].value);
     num2=Number(document.getElementsByName("ohmv")[0].value);
-    document.getElementsByName("Voltaje_Total") [0].innerHTML=Math.round((num1*num2)*100)/100;
+
+    if(isNaN(document.getElementsByName("Voltaje_Total") [0].innerHTML=Math.round((num1*num2)*100)/100)){
+        alert("Se ingreso un valor invalido");}
 }
 
+/**
+ * Calcula la corriente
+ * @method CalcularCorriente
+ */
 function CalcularCorriente() {
     var num1,num2;
 
     num1=Number(document.getElementsByName("voltc")[0].value);
     num2=Number(document.getElementsByName("ohmc")[0].value);
-    document.getElementsByName("Corriente_Total") [0].innerHTML=Math.round((num1/num2)*100)/100 ;
+    if(isNaN(document.getElementsByName("Corriente_Total") [0].innerHTML=Math.round((num1*num2)*100)/100)){
+        alert("Se ingreso un valor invalido");}
 }
 
+/**
+ * Calcula la resistencia
+ * @method CalcularResistencia
+ */
 function CalcularResistencia() {
     var num1,num2;
 
     num1=Number(document.getElementsByName("voltr")[0].value);
     num2=Number(document.getElementsByName("ampr")[0].value);
-    document.getElementsByName("Resistencia_Total") [0].innerHTML=Math.round((num1/num2)*100)/100
+    if(isNaN(document.getElementsByName("Resistencia_Total") [0].innerHTML=Math.round((num1/num2)*100)/100)){
+        alert("Se ingreso un valor invalido");}
+
 }
 
-
+/**
+ * Grafica el voltaje
+ * @method GraficarVoltaje
+ * @param posX
+ * @param posY
+ */
 function GraficarVoltaje(posX,posY) {
 
     var canvas = document.getElementById("Canvas");
@@ -42,6 +64,12 @@ function GraficarVoltaje(posX,posY) {
 
 }
 
+/**
+ * Grafica la corriente
+ * @method GraficarCorriente
+ * @param posX
+ * @param posY
+ */
 function GraficarCorriente(posX,posY) {
 
     var canvas = document.getElementById("Canvas");
@@ -61,6 +89,12 @@ function GraficarCorriente(posX,posY) {
 
 }
 
+/**
+ * Grafica la resitencia
+ * @method GraficarResistencia
+ * @param posX
+ * @param posY
+ */
 function GraficarResistencia(posX,posY) {
 
     var canvas = document.getElementById("Canvas");
